78616ff4ee58eed8eab33eab0ef959c193effcf7
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2025-04-25 10:52:38 +0200

    feat: add .env file for API URL and update Vite config to load environment variables

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,8 +1,12 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    define: {
+      "process.env": env,
+    },
+  };
 });
