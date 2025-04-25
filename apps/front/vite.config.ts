c502f40558e75622c341bf0ab64481629dbbae5d
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2025-04-25 10:53:52 +0200

    fix: Vite configuration

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,4 +1,7 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, loadEnv } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
